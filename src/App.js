b330e85db5411adeed743f12be3da7762f59b58e
jason <binglong2206@gmail.com>
2022-07-23 18:51:05 +0700

    set up custom theemes for chakra provider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
+import React from "react";
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from './themes'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn dog
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={theme} resetCss={false}>
+      <div className="App">Hellow World</div>
+    </ChakraProvider>
   );
 }
 
