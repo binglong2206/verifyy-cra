797bbfdc8181694ae52f820728aeaee962c7e0cd
jason <binglong2206@gmail.com>
2022-07-24 20:46:49 +0700

    nav bar layout

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,79 @@
+// Chakra Imports
+import {
+  Box,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Flex,
+  Link,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import AdminNavbarLinks from "./AdminNavbarLinks";
+
+export default function Navbar({onOpen}) {
+
+  // Some default chakra colors, easier to change later
+  let mainText = useColorModeValue("gray.700", "gray.200");
+  let secondaryText = useColorModeValue("gray.400", "gray.200");
+  let navbarPosition = "absolute";
+  let navbarFilter = "none";
+  let navbarBackdrop = "none"; //blur(21px)
+  let navbarShadow = "none";
+  let navbarBg = "none";
+  let navbarBorder = "transparent";
+  let secondaryMargin = "22px"; // 0px
+  let paddingX = "30px";
+
+
+  return (
+    <Flex
+      position={navbarPosition}
+      boxShadow={navbarShadow}
+      bg={navbarBg}
+      borderColor={navbarBorder}
+      filter={navbarFilter}
+      backdropFilter={navbarBackdrop}
+      borderWidth="1.5px"
+      borderStyle="solid"
+      transitionDelay="0s, 0s, 0s, 0s"
+      transitionDuration=" 0.25s, 0.25s, 0.25s, 0s"
+      transition-property="box-shadow, background-color, filter, border"
+      transitionTimingFunction="linear, linear, linear, linear"
+      alignItems={{ xl: "center" }}
+      borderRadius="16px"
+      display="flex"
+      minH="75px"
+      justifyContent={{ xl: "center" }}
+      lineHeight="25.6px"
+      mx="auto"
+      mt={secondaryMargin}
+      pb="8px"
+      left={document.documentElement.dir === "rtl" ? "30px" : ""}
+      right={document.documentElement.dir === "rtl" ? "" : "30px"}
+      px={{
+        sm: paddingX,
+        md: "30px",
+      }}
+      ps={{
+        xl: "12px",
+      }}
+      pt="8px"
+      top="18px"
+      w={{ sm: "calc(100vw - 30px)", xl: "calc(100vw - 75px - 275px)" }}
+    >
+      <Flex
+        w="100%"
+        flexDirection={{
+          sm: "column",
+          md: "row",
+        }}
+        alignItems={{ xl: "center" }}
+      >
+        
+        <div>menu list</div>
+      </Flex>
+    </Flex>
+  );
+}
+
