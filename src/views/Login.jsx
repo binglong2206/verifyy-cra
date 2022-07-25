0d263b02bca2c52d51197490a69032fcd3233b41
jason <binglong2206@gmail.com>
2022-07-26 00:04:53 +0700

    login navbar

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { Box, Portal } from "@chakra-ui/react";
 import Footer from '../components/Footer'
+import LoginNavBar from "../components/Navbar/LoginNavBar";
 
 export default function Pages() {
   const navRef = React.useRef();
   const wrapper = React.createRef();
   React.useEffect(() => {
     document.body.style.overflow = "unset";
-    // Specify how to clean up after this effect:
     return function cleanup() {};
   });
 
   return (
       <Box ref={navRef} w="100%">
         <Portal containerRef={navRef}>
-        <div>login nav bar</div>
+        <LoginNavBar />
         </Portal>
         <Box w="100%">
-          <Box ref={wrapper} w="100%">
+          <Box ref={wrapper} w="100%" pt={900}>
            <div>sign in component</div>
           </Box>
         </Box>
