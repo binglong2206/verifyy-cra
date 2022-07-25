68ff0edb76b83c2642f537a79205940dde3c2884
jason <binglong2206@gmail.com>
2022-07-25 23:48:03 +0700

    login page layout

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,12 +1,29 @@
 import React from "react";
-import { Divider } from "@chakra-ui/react";
+import { Box, Portal } from "@chakra-ui/react";
+import Footer from '../components/Footer'
 
+export default function Pages() {
+  const navRef = React.useRef();
+  const wrapper = React.createRef();
+  React.useEffect(() => {
+    document.body.style.overflow = "unset";
+    // Specify how to clean up after this effect:
+    return function cleanup() {};
+  });
 
-const Login = () => {
-    return (
-        <div>Hellow World</div>
-    )
+  return (
+      <Box ref={navRef} w="100%">
+        <Portal containerRef={navRef}>
+        <div>login nav bar</div>
+        </Portal>
+        <Box w="100%">
+          <Box ref={wrapper} w="100%">
+           <div>sign in component</div>
+          </Box>
+        </Box>
+        <Box px="24px" mx="auto" width="1044px" maxW="100%">
+          <Footer />
+        </Box>
+      </Box>
+  );
 }
-
-
-export default Login
