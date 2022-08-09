9d0f9289a054096c076b3b1ab5ed07a4375e3221
jason <binglong2206@gmail.com>
2022-08-09 17:33:43 +0700

    state insert for overview rows

--- src/components/charts/overview/index.jsx
+++ src/components/charts/overview/index.jsx
@@ -2,34 +2,42 @@ import React from "react";
 import { SimpleGrid, useColorModeValue } from "@chakra-ui/react";
 import { GlobeIcon } from "../../Icons/Icons";
 import StatsCard from "./StatsCard";
+import {useUserStore, useYoutubeStore, useInstagramStore, useFacebookStore} from '../../../state/useStore'
+import {BsYoutube, BsFacebook, BsInstagram, BsFillPersonCheckFill} from 'react-icons/bs'
 
 
 const OverviewChart = () => {
-    const iconBoxInside = useColorModeValue("white", "blue");
+    const iconBoxInside = useColorModeValue("white", "black");
+    const user = useUserStore(state => state)
+    const youtube = useYoutubeStore(state=>state);
+    const instagram = useInstagramStore(state=>state);
+    const facebook = useFacebookStore(state=>state)
+
+
+
 return (
     <SimpleGrid columns={{ sm: 1, md: 2, xl: 4 }} spacing='24px' mb={'26px'}>
         <StatsCard
           title={"Total Followers"}
-          amount={"53,000"}
-          icon={<GlobeIcon h={"24px"} w={"24px"} color={iconBoxInside} />}
+          amount={user.follower_count ? user.follower_count : 0}
+          icon={<BsFillPersonCheckFill h={"24px"} w={"24px"} color={iconBoxInside} />}
         />
         <StatsCard
           title={"Youtube's Subscribers"}
-          amount={"2,300"}
-          percentage={5}
-          icon={<GlobeIcon h={"24px"} w={"24px"} color={iconBoxInside} />}
+          amount={youtube.follower_count}
+          // percentage={5}
+          icon={<BsYoutube h={"24px"} w={"24px"} color={iconBoxInside} />}
         />
         <StatsCard
           title={"Instagram Followers"}
-          amount={"3,020"}
-          percentage={14}
-          icon={<GlobeIcon h={"24px"} w={"24px"} color={iconBoxInside} />}
+          amount={instagram.follower_count}
+          icon={<BsInstagram h={"24px"} w={"24px"} color={iconBoxInside} />}
         />
         <StatsCard
           title={"Facebook Fans"}
-          amount={"173,000"}
-          percentage={8}
-          icon={<GlobeIcon h={"24px"} w={"24px"} color={iconBoxInside} />}
+          amount={facebook.follower_count}
+          // percentage={8}
+          icon={<BsFacebook h={"24px"} w={"24px"} color={iconBoxInside} />}
         />
       </SimpleGrid>
 )   
